64afbda358e9db8ee9dbb007752763a2cbcd3d13
arsh-deep-0 <96560863+arsh-deep-0@users.noreply.github.com>
2024-05-10 03:07:34 +0530

    wrapped useseaerch params in suspense boundary

--- client/src/app/course-details/page.tsx
+++ client/src/app/course-details/page.tsx
@@ -2,13 +2,15 @@ import CartOptions from "@/components/course-details.tsx/cart-options";
 import OtherDetails from "@/components/course-details.tsx/other-details";
 import Overview from "@/components/course-details.tsx/overview";
 import Syllabus from "@/components/course-details.tsx/syllabus";
-import React from "react";
+import React, { Suspense } from "react";
 
 
 export default function Page() {
   return (
     <div className="min-h-screen bg-light-blue flex flex-col  w-full gap-4 poppins text-black p-4 md:px-24 md:py-16">
+      <Suspense fallback={<div>Loading...</div>}>
       <Overview />
+    </Suspense>
       <CartOptions />
       <OtherDetails />
       <Syllabus/>
